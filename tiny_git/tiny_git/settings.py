0d4fd1306453127f38b185ad2761807ebe24d16e
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2021-01-27 23:50:48 +0100

    feat: User Registration

--- tiny_git/tiny_git/settings.py
+++ tiny_git/tiny_git/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'git.apps.GitConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
