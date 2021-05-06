594bc6d04db96b33c6f6ec70823115a674a5df6f
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2021-05-06 19:19:09 +0200

    feat: Update User Profile

--- tiny_git/users/models.py
+++ tiny_git/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #if the user is deleted also delete the profile
@@ -8,3 +9,12 @@ class Profile(models.Model):
     def __str__(self): #how we want profile to be displayed
         return f'{self.user.username} Profile'
 
+    def save(self): #overriding save method
+        super().save()
+
+        img = Image.open(self.image.path) #open the image of the current instance
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
